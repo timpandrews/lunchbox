ea603d1ba55d0472e43e02960342d22aa1afdf1c
Tim Andrews <tim.andrews@imagineifinc.com>
2017-01-05 17:47:49 -0500

    create following table

--- gardenList/admin.py
+++ gardenList/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from gardenList.models import following
+
+admin.site.register(following)
--- gardenList/models.py
+++ gardenList/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class following(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
+    followingID = models.IntegerField()
+
+    def __unicode__(self):
+        return self.title
