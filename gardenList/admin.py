df34937990714eff88fe4dc3f3cf919f3bf5a0ca
Tim Andrews <tim.andrews@imagineifinc.com>
2017-01-12 12:22:11 -0500

    start on gardeners list page

--- gardenList/admin.py
+++ gardenList/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from gardenList.models import following
 
-admin.site.register(following)
+class followingAdmin(admin.ModelAdmin):
+    class Meta:
+        model = following
+
+    list_display = [
+        "account",
+        "following",
+    ]
+
+    ordering = ["account", "following"]
+
+
+admin.site.register(following, followingAdmin)
--- gardenList/models.py
+++ gardenList/models.py
@@ -7,9 +7,12 @@ class following(models.Model):
     account = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='account', default=1)
     following = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='following', default=1)
 
-    def __str__(self):
+
+    def __unicode__(self): # python 2
         return str(self.account)
 
-    def __unicode__(self):
+    def __str__(self): # python 3
         return str(self.account)
+
+
 
